e083440910d5565cc994348709e90a75aa47d0c9
ameliesiquet <amelie.siquet@student.hepl.be>
2023-10-09 20:34:21 +0200

    test

--- OCR-starter/chapitre1_exercice3.js
+++ OCR-starter/chapitre1_exercice3.js
@@ -14,4 +14,21 @@ avec les valeurs 4 et 5 selon qu'elles sont de type nombre ou de type chaîne  :
 - que se passe-t-il si on entoure toute l'expression (VALEUR OPÉRANDE VALEUR) de guillemets ? Pourquoi ?
 */
 
+let nummer1 =4;
+let nummer2 =5;
+let resultatnombres= nummer1+nummer2;
+console.log("resultat nombres"+resultatnombres); //9
+
+let chaine1=4;
+let chaine2=5;
+let resultatchaines= chaine1+chaine2;
+console.log("resultat chaines"+ resultatchaines); //45
+
+let chaine=4;
+let nummer=5;
+let resultatchainenombre= chaine+nummer;
+console.log("resultat chaine et nombre"+resultatchainenombre); //45?
+
+let guillemets= "4"+"5";
+console.log("resultat guillemets"+ guillemets); //45, car "4" et "5" sont traitées comme des chaines de caractères et plus comme des nombres
 
