3a19914b4a2d1a769e193c2e5ccd9301fda01606
ameliesiquet <amelie.siquet@student.hepl.be>
2023-10-09 20:15:00 +0200

    test

--- OCR-starter/chapitre1_exercice2.js
+++ OCR-starter/chapitre1_exercice2.js
@@ -9,3 +9,11 @@ Affichez dans la console les opérations de base avec les nombres 6 et 3 :
 Avant d'exécuter le code, devinez à chaque fois le résultat de l'opération
 */
 
+const nombre1= 6;
+const nombre2 =3
+
+console.log("somme"+ (nombre1+nombre2)); //9
+console.log("différence"+(nombre1-nombre2)); //3
+console.log("multiplication"+(nombre1*nombre2)); //18
+console.log("divison"+(nombre1/nombre2)); //2
+console.log("modulo"+ (nombre1%nombre2));
