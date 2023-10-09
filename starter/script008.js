214d996c3030d4d5d53c0028cb4ce78da74c30f7
ameliesiquet <amelie.siquet@student.hepl.be>
2023-10-09 19:56:50 +0200

    test

--- starter/script008.js
+++ starter/script008.js
@@ -3,18 +3,23 @@
 */
 
 // 1. Initialiser une variable firstName à John et afficher sa valeur dans la console
-
+const firstName = "John";
 
 // 2. Initialiser une variable lastName à Smith et une variable age à 28
-
+const lastName = "Smith";
 
 // 3. Initialiser une variable fullAge à true et l'afficher
-
+const fullAge ="true"
 
 // 4. Déclarer une variable job et afficher sa valeur dans la console
+// Déclaration de la variable job
+let job ="Enseignant";
+console.log(job);
 
 
 // 5. Affecter la valeur Enseignat à la variable job et l'afficher 
+// Affectation de la valeur "Enseignant" à la variable job
+job = "Enseignant";
 
 
 // 6. Règles de nommage des variables : le nom ne peut pas 
