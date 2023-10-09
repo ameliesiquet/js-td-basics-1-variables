8c3da787ee79ea9a68fa99a58962e95593284abc
ameliesiquet <amelie.siquet@student.hepl.be>
2023-10-09 20:52:01 +0200

    test

--- starter/script010.js
+++ starter/script010.js
@@ -4,7 +4,10 @@
 
 // 1. Déclarer les variables now, ageJohn, ageMark, yearJohn et yearMark
 // puis affecter aux trois premières les valeurs 2019, 28 et 33
-
+const now, ageJohn, ageMark, yearJohn, yearMark;
+now= 2019;
+ageJohn= 28;
+ageMark= 33;
 
 // Opérateurs mathématiques
 
@@ -13,12 +16,24 @@
 // idem pour yearMark
 // puis afficher la date de naissance de John 
 // et "Mark est né en …"
+yearJohn= 1991;
+yearMark= 1986;
+
+console.log("date de naissance de John", yearJohn);
+console.log("Mark est né en …",yearMark);
 
 
 // 3. Afficher l'année à laquelle nous serons dans deux ans
 // le double de l'année
 // le dixième de l'année
+const anneendeuxans= now+2;
+console.log("anné dans deux ans", anneendeuxans);
 
+const doubleanne= anneendeuxans*2;
+console.log("le double de l'anné", doubleanne);
+
+const dixiemeanne= anneendeuxans/10;
+console.log("le dixième de l'anné", dixiemeanne);
 
 
 // Opérateurs logiques
